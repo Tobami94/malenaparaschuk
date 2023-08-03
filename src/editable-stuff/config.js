805eda053fedd98dd3374ad6ed394f7ec7f9f0b6
Tobami <malenaparaschuk@gmail.com>
2023-08-03 10:13:43 +0200

    se modifican enlaces

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -36,7 +36,7 @@ const about = {
   imageSize: 375,
   message:
     "Soy una apasionada desarrolladora full stack web con un enfoque especializado en el front end.  Me encanta traducir ideas creativas en código, creando interfaces hermosas y funcionales que cautivan a los usuarios y los mantienen comprometidos.  Me considero una persona alegre y comunicativa por naturaleza. Disfruto colaborar en equipos, compartir ideas y encontrar soluciones creativas a los desafíos que enfrentamos. En mi tiempo libre me dedico a cuidar plantas y animales, amo la naturaleza tanto como el mundo tech.",
-  resume: "https://docs.google.com/document/d/18Y7W_B8vPy4NYwOqxWOUnL0SuDPfvwOUMQ_Vzsu8ccw/edit",
+  resume: "https://drive.google.com/file/d/1bygbmyUHOJpEpzrxEeW5IDrYMUAoNhnl/view",
 };
 
 

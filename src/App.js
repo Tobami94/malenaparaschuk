38381184e77af9f36e72559ae17deaca150bc1ed
unknown <malenaparaschuk@gmail.com>
2024-05-05 13:19:15 +0200

    root

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const App = () => {
   const titleRef = React.useRef();
 
   return (
-    <BrowserRouter basename="Tobami94/malenaparaschuk">
+<>
       {navBar.show && <Navbar ref={titleRef} />}
       <Routes>
         <Route path="/" exact element={<Home ref={titleRef} />} />
@@ -83,7 +83,7 @@ const App = () => {
           />
         )}
       </Footer>
-    </BrowserRouter>
+      </>
   );
 };
 
